85c4145d844951528cc019f9ff3d20fce177bbd6
Mihai Hiticas <hiticasmihai7@gmail.com>
2026-02-07 11:58:18 +0200

    connect fe to be, fetch data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [invoices, setInvoices] = useState([]);
+
+  useEffect(() => {
+    fetch("https://invoice-app-rust-five.vercel.app/api/invoices")
+      .then((res) => res.json())
+      .then((data) => setInvoices(data))
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Invoices</h1>
+
+      {invoices.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        invoices.map((inv) => (
+          <div key={inv._id} className="invoice">
+            <h3>{inv.title}</h3>
+
+            {/* Format date */}
+            <p>
+              Created:{" "}
+              {new Date(inv.createdAt).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+          </div>
+        ))
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
